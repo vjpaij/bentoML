48dfd9c0c81ef1b4bafbb36faf5af17da56bcb8b
Vijay Pai <vijay.pai@tesco.com>
2024-11-17 23:04:31 +0530

    add bentoml folder created in local system

---
+++ bentoml/bentos/iris_classifier/m235vjvfa2m4huwy/src/service.py
@@ -0,0 +1,19 @@
+import numpy as np
+import bentoml
+from bentoml.io import NumpyNdarray
+
+iris_clf_runner=bentoml.sklearn.get("iris_clf:latest").to_runner()
+svc=bentoml.Service("iris_classifier",runners=[iris_clf_runner])
+
+@svc.api(input=NumpyNdarray(),output=NumpyNdarray())
+def classify(input_series: np.ndarray) -> np.ndarray:
+    result=iris_clf_runner.predict.run(input_series)
+    return result
+
+#To run the above code in terminal:
+#command: bentoml serve  service.py:svc --reload
+#it creates a swagger url
+
+#to build the project, create bentoml.yaml
+#command: bentoml build 
+
